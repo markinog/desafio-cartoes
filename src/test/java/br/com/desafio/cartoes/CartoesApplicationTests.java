c92af15fdf3fa5a6b57431374f8a7a78be057f23
markinog <marcussla9@gmail.com>
2026-07-26 19:51:50 -0300

    first commit

---
+++ src/test/java/br/com/desafio/cartoes/CartoesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.desafio.cartoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CartoesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
